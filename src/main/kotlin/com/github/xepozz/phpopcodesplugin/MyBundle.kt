da7a28993cc21d94a8fa8751a100bc328a856519
GitHub Action <action@github.com>
2025-07-04 16:27:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/xepozz/phpopcodesplugin/MyBundle.kt
+++ src/main/kotlin/com/github/xepozz/phpopcodesplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.xepozz.phpopcodesplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/xepozz/phpopcodesplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/xepozz/phpopcodesplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.xepozz.phpopcodesplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xepozz.phpopcodesplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
