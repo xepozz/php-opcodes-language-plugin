35862e07dd3348df1894969fa60eac2ddf96d628
Dmitriy Derepko <xepozz@list.ru>
2025-07-05 11:53:59 +0300

    feat: highlight tokens

--- src/main/kotlin/com/github/xepozz/php_opcodes_language/language/PHPOpAnnotator.kt
+++ src/main/kotlin/com/github/xepozz/php_opcodes_language/language/PHPOpAnnotator.kt
@@ -1,35 +1,30 @@
 package com.github.xepozz.php_opcodes_language.language
 
+import com.github.xepozz.php_opcodes_language.language.psi.PHPOpInstructionName
+import com.github.xepozz.php_opcodes_language.language.psi.PHPOpVarName
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
-import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.psi.PsiElement
+import com.jetbrains.php.lang.highlighter.PhpHighlightingData
 
 class PHPOpAnnotator : Annotator {
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-    }
+//        println("Annotating $element: ${element.text}")
+        when (element) {
+            is PHPOpVarName -> {
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                    .range(element)
+                    .textAttributes(PhpHighlightingData.VAR)
+                    .create()
+            }
 
-    companion object {
-        val PATTERN_HIGHLIGHT = TextAttributesKey.createTextAttributesKey(
-            "PHP_OPCODES_LANGUAGE_PATTERN",
-            DefaultLanguageHighlighterColors.STRING,
-        )
-        val REFERENCE_DECLARATION_HIGHLIGHT = TextAttributesKey.createTextAttributesKey(
-            "PHP_OPCODES_LANGUAGE_DIRECTIVE",
-            DefaultLanguageHighlighterColors.FUNCTION_DECLARATION,
-        )
-        val REFERENCE_USAGE_HIGHLIGHT = TextAttributesKey.createTextAttributesKey(
-            "PHP_OPCODES_LANGUAGE_ARGUMENT",
-            DefaultLanguageHighlighterColors.FUNCTION_CALL,
-        )
-        val BLOCK_NAME_HIGHLIGHT = TextAttributesKey.createTextAttributesKey(
-            "PHP_OPCODES_LANGUAGE_BLOCK_NAME",
-            DefaultLanguageHighlighterColors.CLASS_NAME,
-        )
-        private val IDENTIFIER_HIGHLIGHT = TextAttributesKey.createTextAttributesKey(
-            "PHP_OPCODES_LANGUAGE_IDENTIFIER",
-            DefaultLanguageHighlighterColors.KEYWORD,
-        )
+            is PHPOpInstructionName -> {
+                holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
+                    .range(element)
+                    .textAttributes(PhpHighlightingData.FUNCTION_CALL)
+                    .create()
+            }
+        }
     }
 }
--- src/main/kotlin/com/github/xepozz/php_opcodes_language/language/PHPOpSyntaxHighlighterFactory.kt
+++ src/main/kotlin/com/github/xepozz/php_opcodes_language/language/PHPOpSyntaxHighlighterFactory.kt
@@ -1,9 +1,51 @@
 package com.github.xepozz.php_opcodes_language.language
 
+import com.github.xepozz.php_opcodes_language.language.parser.PHPOpLexerAdapter
+import com.github.xepozz.php_opcodes_language.language.psi.PHPOpTypes
+import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
+import com.intellij.openapi.editor.HighlighterColors
+import com.intellij.openapi.editor.colors.TextAttributesKey
+import com.intellij.openapi.fileTypes.SyntaxHighlighterBase
 import com.intellij.openapi.fileTypes.SyntaxHighlighterFactory
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.TokenType
+import com.intellij.psi.tree.IElementType
 
 internal class PHPOpSyntaxHighlighterFactory : SyntaxHighlighterFactory() {
-    override fun getSyntaxHighlighter(project: Project?, virtualFile: VirtualFile?) = PHPOpSyntaxHighlighter()
+    override fun getSyntaxHighlighter(project: Project?, virtualFile: VirtualFile?) = object : SyntaxHighlighterBase() {
+        override fun getHighlightingLexer() = PHPOpLexerAdapter()
+
+        override fun getTokenHighlights(tokenType: IElementType) = when (tokenType) {
+            PHPOpTypes.NUMBER -> NUMBER_KEYS
+            PHPOpTypes.TEXT -> TEXT_KEYS
+            PHPOpTypes.COMMENT -> COMMENT_KEYS
+            TokenType.BAD_CHARACTER -> BAD_CHAR_KEYS
+            else -> EMPTY_KEYS
+        }
+    }
+
+    companion object {
+        private val BAD_CHAR_KEYS = arrayOf(
+            HighlighterColors.BAD_CHARACTER,
+        )
+
+        private val COMMENT_KEYS = arrayOf(
+            DefaultLanguageHighlighterColors.DOC_COMMENT
+        )
+        private val TEXT_KEYS = arrayOf(
+            DefaultLanguageHighlighterColors.STRING
+        )
+        private val NUMBER_KEYS = arrayOf(
+            DefaultLanguageHighlighterColors.NUMBER
+        )
+        private val HEREDOC_MARKER_KEYS = arrayOf(
+            DefaultLanguageHighlighterColors.KEYWORD
+        )
+        private val HEREDOC_CONTENT_KEYS = arrayOf(
+            DefaultLanguageHighlighterColors.STRING
+        )
+        private val EMPTY_KEYS = emptyArray<TextAttributesKey>()
+    }
+
 }
